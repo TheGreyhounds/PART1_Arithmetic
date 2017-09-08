2149244081ffab04c618b362ea3328d425a48334
TheGentlemanCoder <santasmith123@hotmail.com>
2017-09-07 22:26:55 -0700

    Create Arithmetic code and save work so far

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        // addition is with the '+' operator
+        
+        System.out.println("Two plus two is equal to " + (2 + 2));
+        System.out.println("362 plus 781 is equal to " + (363 + 781));
+        System.out.println("999,999,999 + 1 is equal to " + (999999999 + 1));
+        
+        // subtraction is with the '-' operator
+        
+        System.out.println("Two minus two is equal to " + (2 - 2));
+        System.out.println("121 minus 459 is equal to " + (121 - 459));
+        System.out.println("1 - 999,999,999 is equal to " + (1 - 999999999));
+        
+        // multiplication is with the '*' operator
+        System.out.println("Two times two is equal to " + (2 * 2));
+        System.out.println("7 times by 7 times by 7 is " + (7 * 7 * 7));
+        System.out.println("1,000 x by 0 is equal to" + (1000 * 0));
+        
+        // division is with the '/' operator
+        System.out.println("1 divided by 1 is equal to " + (1 / 1));
+        System.out.println("1 divided by 2 is equal to " + (1 / 2));
+        System.out.println("5 / 6 is " + (5 / 6));
+        System.out.println("6 / 5 is " + (6 / 5));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PART1_Arithmetic
-Java is very similar to other programming languages in its syntax of arithmetic, so doing maths won't be so bad
+Java is very similar to other programming languages in its syntax of arithmetic, so doing maths won't be so bad.
