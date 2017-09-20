793f376cf4b3890a8983bcdecfbace2ba1023a57
TheGentlemanCoder <santasmith123@hotmail.com>
2017-09-19 20:38:59 -0700

    Finish code snippet demonstrating arithmetic operators and assignment operators, and complete README explanation

--- Arithmetic.java
+++ Arithmetic.java
@@ -4,7 +4,9 @@ public class Arithmetic {
         
         System.out.println("Two plus two is equal to " + (2 + 2));
         System.out.println("362 plus 781 is equal to " + (363 + 781));
-        System.out.println("999,999,999 + 1 is equal to " + (999999999 + 1));
+        System.out.println("999,999,999 + 1 is equal to " + (999_999_999 + 1)); // <-- Notice again it's perfectly
+                                                                       // allowed to have underscores in our integer
+                                                                       // literals to make them more readable.
         
         // subtraction is with the '-' operator
         
@@ -15,12 +17,61 @@ public class Arithmetic {
         // multiplication is with the '*' operator
         System.out.println("Two times two is equal to " + (2 * 2));
         System.out.println("7 times by 7 times by 7 is " + (7 * 7 * 7));
-        System.out.println("1,000 x by 0 is equal to" + (1000 * 0));
+        System.out.println("1,000 x by 0 is equal to " + (1000 * 0));
         
         // division is with the '/' operator
         System.out.println("1 divided by 1 is equal to " + (1 / 1));
         System.out.println("1 divided by 2 is equal to " + (1 / 2));
         System.out.println("5 / 6 is " + (5 / 6));
         System.out.println("6 / 5 is " + (6 / 5));
+        
+        
+        // Pay close attention to these next expressions.
+        
+        
+        
+        int num1 = 6;
+        int num2 = 12;
+        
+        num2 = num1 * num2;
+        System.out.println("num1: " + num1 + " num2: " + num2);
+        
+        num1 = 6;
+        // Did I have to reset the value of num1 to 6? 
+        num2 = 12;
+        num2 *= num1;
+        //   ^^ This is called an "assignment operator". What do you think it does?
+        System.out.println("num1: " + num1 + " num2: " + num2);
+        
+        num2 = 12;
+        num2 += num2;
+        // You can reference a variable twice in the same expression?!?
+        System.out.println("num1: " + num1 + " num2: " + num2);
+        
+        num2 = 12;
+        num2 -= num1;
+        System.out.println("num1: " + num1 + " num2: " + num2);
+        
+        num2 = 12;
+        num2 /= num1;
+        System.out.println("num1: " + num1 + " num2: " + num2);
+        
+        
+        
+        int newNum = 5;
+        
+        newNum++;
+        System.out.println("newNum: " + newNum);
+        newNum++;  
+        System.out.println("newNum: " + newNum);
+        newNum++;   // These are called increment operators, and are equivalent to
+                    // the expression "num += 1;". Also, yeah, in-line comments are a thing :)            
+        System.out.println("newNum: " + newNum);
+        
+        newNum--;
+        System.out.println("newNum: " + newNum);
+        newNum--;   // These are called decrement operators, and are equivalent to
+                    // the expression "num -= 1;".
+        System.out.println("newNum: " + newNum);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PART1_Arithmetic
 Java is very similar to other programming languages in its syntax of arithmetic, so doing maths won't be so bad.
+
+To manipulate numerical values, as almost any program you write will have to, Java - like any programming language -
+provides standard "arithmetic operators," which perform all of your basic math functions.
+
+Arithmetic Operators:
+- Addition: '+'
+- Subtraction: '-'
+- Multiplication: '*'
+- Division '/'
+
+This code snippet contained more than arithmetic operators, however; it also contained *assignment operators*.
+Their form is very similar to to the arithmetic operators. You just have to remember the fundamental difference
+between arithmetic operators and assignment operators: arithmetic operators perform their mathematical function,
+assignment operators perform their mathematical function *and* save it to the variable. Their syntax is shown below.
+
+Assignment Operators:
+- Addition: "+="
+- Subtraction: "-="
+- Multiplication: "*="
+- Division "/="
+
+Since arithmetic is something you've done since preschool, the critical takeaway from this section is recognizing that 
+Java doesn't store functions, it stores value. If you set variable 'A' to some value that uses variable 'B', and later 
+change variable 'B' to a different value, variable 'A' still stores the exact same value.
