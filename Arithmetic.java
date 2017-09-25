f3778cfe086b94222d005ad5a2a3c96c31d882f0
SMITHNIC <SMITHNIC@psd267.wednet.edu>
2017-09-25 09:15:12 -0700

    Add modulo operator to code snippet, and updated the README to explain its function.

--- Arithmetic.java
+++ Arithmetic.java
@@ -6,28 +6,40 @@ public class Arithmetic {
         System.out.println("362 plus 781 is equal to " + (363 + 781));
         System.out.println("999,999,999 + 1 is equal to " + (999_999_999 + 1)); // <-- Notice again it's perfectly
                                                                        // allowed to have underscores in our integer
-                                                                       // literals to make them more readable.
-        
+        System.out.println("");                                        // literals to make them more readable.
+		
         // subtraction is with the '-' operator
         
         System.out.println("Two minus two is equal to " + (2 - 2));
         System.out.println("121 minus 459 is equal to " + (121 - 459));
         System.out.println("1 - 999,999,999 is equal to " + (1 - 999999999));
-        
+        System.out.println("");
+		
         // multiplication is with the '*' operator
         System.out.println("Two times two is equal to " + (2 * 2));
         System.out.println("7 times by 7 times by 7 is " + (7 * 7 * 7));
         System.out.println("1,000 x by 0 is equal to " + (1000 * 0));
-        
+        System.out.println("");
+		
         // division is with the '/' operator
         System.out.println("1 divided by 1 is equal to " + (1 / 1));
         System.out.println("1 divided by 2 is equal to " + (1 / 2));
         System.out.println("5 / 6 is " + (5 / 6));
         System.out.println("6 / 5 is " + (6 / 5));
+		System.out.println("");
+		
+		// modulus arithmetic with the '%' operator.
+		// This operator returns the remainder of the division
+		System.out.println("-8 % 3 is " + (-8 % 3));
+		System.out.println("8 % 4 is " + (8 % 4));
+		System.out.println("4 % 8 is " + (4 % 8));
+		System.out.println("9 % 3 is " + (9 % 3));
+		System.out.println("-257 % 2 is " + (-257 % 2));
+		System.out.println("-99 % -10 is " + (-99 % -10));
+		System.out.println("10 % 99 is " + (10 % 99));
         
-        
+		System.out.println("");
         // Pay close attention to these next expressions.
-        
         
         
         int num1 = 6;
@@ -55,7 +67,12 @@ public class Arithmetic {
         num2 = 12;
         num2 /= num1;
         System.out.println("num1: " + num1 + " num2: " + num2);
+		
+		num2 = 12;
+		num1 %= num2;
+		System.out.println("num1: " + num1 + " num2: " + num2);
         
+		System.out.println("");
         
         
         int newNum = 5;
--- README.md
+++ README.md
@@ -9,6 +9,14 @@ Arithmetic Operators:
 - Subtraction: '-'
 - Multiplication: '*'
 - Division '/'
+- Modular '%'
+
+All of these should be familiar to you, except perhaps the modulo: '%'. This operator returns the remainder of a
+division, not the division itself. For example, say we told Java to find "7 % 3". 3 goes into 7 evenly two times,
+so we subtract 3 x 2 = 6 from 7 and get 1. That is our answer.
+
+If the second number is larger than the first, the the modulo operator will simply return the first number, because
+if the second number can't even go once into the first, then there's nothing to subtract. For example, "3 % 7" returns 3.
 
 This code snippet contained more than arithmetic operators, however; it also contained *assignment operators*.
 Their form is very similar to to the arithmetic operators. You just have to remember the fundamental difference
@@ -20,7 +28,20 @@ Assignment Operators:
 - Subtraction: "-="
 - Multiplication: "*="
 - Division "/="
+- Modulo "%="
 
 Since arithmetic is something you've done since preschool, the critical takeaway from this section is recognizing that 
-Java doesn't store functions, it stores value. If you set variable 'A' to some value that uses variable 'B', and later 
-change variable 'B' to a different value, variable 'A' still stores the exact same value.
+Java doesn't store functions, it stores value. If you set variable 'A' to some value that uses variable 'B' to initialize its
+value, and later change variable 'B' to a different value, variable 'A' still stores the exact same value; it does not change
+because variable 'B' was changed. You were also likely first introduced to the idea of modular arithmetic in this code snippet, 
+and understanding its use is another key takeaway from this repository. If modular arithmetic still confuses you, feel free to 
+play around with different modulo expressions and try to get a feel for it. A general formula that may help you is that:
+'''
+// The "| |" marks signify absolute value
+if |a| > |b|:
+	a % b = a - (b * n) // where n is the largest integer that also makes |a| > |b * n| true
+if |a| < |b|:
+	a % b = a
+if |a| == |b|:
+	a % b = 0
+'''
